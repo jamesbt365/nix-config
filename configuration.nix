5c33deb3a11a968ed8757f0dcf67f335af3650d3
jamesbt365 <jamesbt365@gmail.com>
2022-01-13 18:14:11 +0000

    Add Configuration

---
+++ configuration.nix
@@ -0,0 +1,107 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+   nix.package = pkgs.nixFlakes;
+   nix.extraOptions = ''
+     experimental-features = nix-command flakes
+     '';
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  networking.hostName = "hentai"; # I'm very funny
+
+  # Set your time zone.
+  time.timeZone = "";
+
+  # honestly mate fuck you
+  boot.extraModulePackages = with config.boot.kernelPackages; [ rtl8821ce ];
+  boot.kernelModules = [ "rtl8821ce" ];
+
+  # not the best solution but it "works"
+  networking.networkmanager.enable = true;
+
+    # xorg lol
+
+    services.xserver = {
+    enable = true;
+
+    libinput = {
+      enable = true;
+
+      mouse = {
+        accelProfile = "flat";
+      };
+      touchpad = {
+        accelProfile = "flat";
+      };
+    };
+
+  };
+  # sddm cringe
+   services.xserver.displayManager.sddm.enable = true;
+
+  # you know how i need this thing to use my computer without having a mental breakdown
+  services.xserver.windowManager.bspwm.enable = true;
+
+  # Configure keymap in X11
+   services.xserver.layout = "dvorak"; # dvorak haha
+
+  # Temp pipewire shit
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+
+  # shit person
+   users.users.james = {
+     isNormalUser = true;
+     initialPassword = "test";
+     extraGroups = [ "wheel" ];
+   };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  # honestly what was i doing here xd i'm so skill issued lmao kill me please
+   environment.systemPackages = with pkgs; [
+     vim
+     wget git neofetch
+     firefox pciutils
+     bspwm
+     sxhkd
+     dotnet-sdk dotnet-sdk_5
+     dotnet-runtime
+];
+     
+  # mmm unfree software
+  nixpkgs.config.allowUnfree = true;
+
+
+
+  # Enable the OpenSSH daemon.
+   services.openssh.enable = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.05"; # Did you read the comment?
+
+}
+
